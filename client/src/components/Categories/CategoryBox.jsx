e007094da6cf563e5e1224fa8988a182d7c8f1a2
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-04-20 13:22:42 +0600

    using queryString

--- client/src/components/Categories/CategoryBox.jsx
+++ client/src/components/Categories/CategoryBox.jsx
@@ -1,18 +1,33 @@
 import PropTypes from 'prop-types'
+import queryString from 'query-string';
+import { useNavigate } from 'react-router-dom';
 
 const CategoryBox = ({ label, icon: Icon }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    let currentQuery = { category: label };
+
+    const url = queryString.stringifyUrl({
+      url: '/',
+      query: currentQuery
+    })
+    console.log(url);
+    navigate(url);
+  }
   return (
     <div
+      onClick={handleClick}
       className={`flex 
-  flex-col 
-  items-center 
-  justify-center 
-  gap-2
-  p-3
-  border-b-2
-  hover:text-neutral-800
-  transition
-  cursor-pointer`}
+      flex-col 
+      items-center 
+      justify-center 
+      gap-2
+      p-2 xl:p-3 mx-1 xl:mx-auto xl:ml-0
+      border-b-2
+      hover:text-neutral-800
+      transition
+      cursor-pointer`}
     >
       <Icon size={26} />
       <div className='text-sm font-medium'>{label}</div>
