a6e3d66b95f2f00e464f0ab2a912b6b0594a64d0
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-04-22 23:09:10 +0600

    added handleDates

--- client/src/pages/Dashboard/Host/AddRoom.jsx
+++ client/src/pages/Dashboard/Host/AddRoom.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import AddRoomForm from "../../../components/Form/AddRoomForm";
 
 const AddRoom = () => {
+    const [dates, setDates] = useState({
+        startDate: new Date(),
+        endDate: null,
+        key: 'selection'
+    })
+
+    // Date range Handler
+    const handleDates = item => {
+        console.log(item);
+        setDates(item.selection);
+    }
+
     return (
         <div>
             {/* Form */}
-            <AddRoomForm></AddRoomForm>
+            <AddRoomForm dates={dates} handleDates={handleDates}></AddRoomForm>
         </div>
     );
 };
