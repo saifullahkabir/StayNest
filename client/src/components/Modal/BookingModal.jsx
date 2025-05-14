6f7d3e9eef9e2b15fa94ca18fde0bd427f4b420e
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-05-15 01:58:51 +0600

    added manage bookings page

--- client/src/components/Modal/BookingModal.jsx
+++ client/src/components/Modal/BookingModal.jsx
@@ -1,93 +1,110 @@
 import PropTypes from 'prop-types'
 import {
-  Dialog,
-  Transition,
-  TransitionChild,
-  DialogPanel,
-  DialogTitle,
+    Dialog,
+    Transition,
+    TransitionChild,
+    DialogPanel,
+    DialogTitle,
 } from '@headlessui/react'
 import { format } from 'date-fns'
 import { Fragment } from 'react'
 import useAuth from '../../hooks/useAuth'
 
 const BookingModal = ({ closeModal, isOpen, bookingInfo }) => {
-    const {user} = useAuth();
+    const { user } = useAuth();
 
-  return (
-    <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as='div' className='relative z-10' onClose={closeModal}>
-        <TransitionChild
-          as={Fragment}
-          enter='ease-out duration-300'
-          enterFrom='opacity-0'
-          enterTo='opacity-100'
-          leave='ease-in duration-200'
-          leaveFrom='opacity-100'
-          leaveTo='opacity-0'
-        >
-          <div className='fixed inset-0 bg-black bg-opacity-25' />
-        </TransitionChild>
-
-        <div className='fixed inset-0 overflow-y-auto'>
-          <div className='flex min-h-full items-center justify-center p-4 text-center'>
-            <TransitionChild
-              as={Fragment}
-              enter='ease-out duration-300'
-              enterFrom='opacity-0 scale-95'
-              enterTo='opacity-100 scale-100'
-              leave='ease-in duration-200'
-              leaveFrom='opacity-100 scale-100'
-              leaveTo='opacity-0 scale-95'
-            >
-              <DialogPanel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
-                <DialogTitle
-                  as='h3'
-                  className='text-lg font-medium text-center leading-6 text-gray-900'
+    return (
+        <Transition appear show={isOpen} as={Fragment}>
+            <Dialog as='div' className='relative z-10' onClose={closeModal}>
+                <TransitionChild
+                    as={Fragment}
+                    enter='ease-out duration-300'
+                    enterFrom='opacity-0'
+                    enterTo='opacity-100'
+                    leave='ease-in duration-200'
+                    leaveFrom='opacity-100'
+                    leaveTo='opacity-0'
                 >
-                  Review Info Before Reserve
-                </DialogTitle>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>
-                    Room: {bookingInfo.title}
-                  </p>
-                </div>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>
-                    Location: {bookingInfo.location}
-                  </p>
-                </div>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>
-                    Guest: {user?.displayName}
-                  </p>
-                </div>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>
-                    From: {format(new Date(bookingInfo.from), 'PP')} - To:{' '}
-                    {format(new Date(bookingInfo.to), 'PP')}
-                  </p>
-                </div>
+                    <div className='fixed inset-0 bg-black bg-opacity-25' />
+                </TransitionChild>
 
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>
-                    Price: $ {bookingInfo.price}
-                  </p>
+                <div className='fixed inset-0 overflow-y-auto'>
+                    <div className='flex min-h-full items-center justify-center p-4 text-center'>
+                        <TransitionChild
+                            as={Fragment}
+                            enter='ease-out duration-300'
+                            enterFrom='opacity-0 scale-95'
+                            enterTo='opacity-100 scale-100'
+                            leave='ease-in duration-200'
+                            leaveFrom='opacity-100 scale-100'
+                            leaveTo='opacity-0 scale-95'
+                        >
+                            <DialogPanel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+                                <DialogTitle
+                                    as='h3'
+                                    className='text-lg font-medium text-center leading-6 text-gray-900'
+                                >
+                                    Review Info Before Reserve
+                                </DialogTitle>
+                                <div className='mt-2'>
+                                    <p className='text-sm text-gray-500'>
+                                        Room: {bookingInfo.title}
+                                    </p>
+                                </div>
+                                <div className='mt-2'>
+                                    <p className='text-sm text-gray-500'>
+                                        Location: {bookingInfo.location}
+                                    </p>
+                                </div>
+                                <div className='mt-2'>
+                                    <p className='text-sm text-gray-500'>
+                                        Guest: {user?.displayName}
+                                    </p>
+                                </div>
+                                <div className='mt-2'>
+                                    <p className='text-sm text-gray-500'>
+                                        From: {format(new Date(bookingInfo.from), 'PP')} - To:{' '}
+                                        {format(new Date(bookingInfo.to), 'PP')}
+                                    </p>
+                                </div>
+
+                                <div className='mt-2'>
+                                    <p className='text-sm text-gray-500'>
+                                        Price: $ {bookingInfo.price}
+                                    </p>
+                                </div>
+                                <hr className='mt-8 ' />
+                                {/* checkout form */}
+
+                                <div className='flex mt-3 justify-around'>
+                                    <button
+                                        onClick={closeModal}
+                                        type='button'
+                                        className='inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2'
+                                    >
+                                        Cancel
+                                    </button>
+                                    <button
+                                        type='button'
+                                        className='inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2'
+                                        
+                                    >
+                                        Pay {bookingInfo.price}$
+                                    </button>
+                                </div>
+                            </DialogPanel>
+                        </TransitionChild>
+                    </div>
                 </div>
-                <hr className='mt-8 ' />
-                {/* checkout form */}
-              </DialogPanel>
-            </TransitionChild>
-          </div>
-        </div>
-      </Dialog>
-    </Transition>
-  )
+            </Dialog>
+        </Transition>
+    )
 }
 
 BookingModal.propTypes = {
-  bookingInfo: PropTypes.object,
-  closeModal: PropTypes.func,
-  isOpen: PropTypes.bool,
+    bookingInfo: PropTypes.object,
+    closeModal: PropTypes.func,
+    isOpen: PropTypes.bool,
 }
 
 export default BookingModal
--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -14,6 +14,8 @@ import Profile from '../pages/Dashboard/Common/Profile'
 import ManageUsers from '../pages/Dashboard/Admin/ManageUsers'
 import AdminRoute from './AdminRoute'
 import HostRoute from './HostRoute'
+import MyBookings from '../pages/Dashboard/Guest/MyBookings'
+import ManageBookings from '../pages/Dashboard/Host/ManageBookings'
 
 export const router = createBrowserRouter([
   {
@@ -42,6 +44,18 @@ export const router = createBrowserRouter([
       {
         index: true,
         element: <PrivateRoute><Statistics></Statistics></PrivateRoute>,
+      },
+      {
+        path: 'my-bookings',
+        element: <PrivateRoute><MyBookings></MyBookings></PrivateRoute>,
+      },
+      {
+        path: 'manage-bookings',
+        element: <PrivateRoute>
+          <HostRoute>
+            <ManageBookings />
+          </HostRoute>
+        </PrivateRoute>,
       },
       {
         path: 'add-room',
