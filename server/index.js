29cf6eb90ca1521f1169a61381de80fefd7252cf
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-10-22 07:16:19 +0600

    host statistics api

--- server/index.js
+++ server/index.js
@@ -305,6 +305,39 @@ async function run() {
       res.send({ totalUsers, totalRooms, totalBookings, totalPrice, chartData });
     })
 
+    // host statistics
+    app.get('/host-stat', verifyToken, verifyHost, async (req, res) => {
+      const email = req.user.email;
+      const bookingDetails = await bookingsCollection.find(
+        { 'host.email': email },
+        {
+          projection: {
+            date: 1,
+            price: 1,
+          }
+        }
+      ).toArray();
+
+      const totalRooms = await roomsCollection.countDocuments({ 'host.email': email });
+      const totalBookings = bookingDetails?.length;
+      const totalPrice = bookingDetails.reduce((sum, booking) => sum + booking.price, 0);
+      const { timestamp } = await usersCollection.findOne(
+        { email },
+        { projection: { timestamp: 1 } }
+      );
+
+      const chartData = bookingDetails.map(booking => {
+        const day = new Date(booking?.date).getDate();
+        const month = new Date(booking?.date).getMonth() + 1;
+        const data = [`${day}/${month}`, booking?.price];
+        return data;
+      });
+      chartData.unshift(['Day', 'Sales']);
+      // chartData.splice(0, 0, ['Days', 'Sales'])  // another one
+
+      res.send({ totalRooms, totalBookings, totalPrice, chartData, hostSince: timestamp });
+    })
+
 
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
