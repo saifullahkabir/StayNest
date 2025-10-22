9acbdb2e1d1b91e77eb11c9655f12703351089b8
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-10-22 06:56:46 +0600

    added HostStatistics & GuestStatistics page

--- client/src/pages/Dashboard/Common/Statistics.jsx
+++ client/src/pages/Dashboard/Common/Statistics.jsx
@@ -1,12 +1,20 @@
 import useRole from "../../../hooks/useRole";
 import AdminStatistics from "../Admin/AdminStatistics";
+import GuestStatistics from "../Guest/GuestStatistics";
+import HostStatistics from "../Host/HostStatistics";
 
 const Statistics = () => {
-    const [role, isLoading] = useRole();
+    const [role] = useRole();
     return (
         <>
             {
                 role === 'admin' && <AdminStatistics />
+            }
+            {
+                role === 'host' && <HostStatistics />
+            }
+            {
+                role === 'guest' && <GuestStatistics />
             }
         </>
     );
